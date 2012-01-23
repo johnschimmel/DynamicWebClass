956626499f0205ea298276206abffaebb003bf9e
John Schimmel <john.schimmel@gmail.com>
2012-01-22 22:39:57 -0500

    Revert c296b354212c005a33dc7b5d6fbb2000f76f5031^..HEAD

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
 // 2 forward slashes allows a comment
 
 var dog = "Sparky"; // create a variable 'dog' with the value 'Sparky'
+var dogAge = 5; // create a number variable
+var catAge = 2.5; // another a number
+
+var names = ["Sparky","Sheldon","Jasper"]; // create an array variable of names
+var ages = [2,5,3];
