72ef7c2859a87e3d8fd23c946bea2160066d6032
John Schimmel <john.schimmel@gmail.com>
2012-01-22 22:39:37 -0500

    Revert 918107e4ab56fee4bd5df1b7e61bab00b724d0df^..HEAD

--- javascript.js
+++ javascript.js
@@ -1,15 +1,3 @@
 // 2 forward slashes allows a comment
 
 var dog = "Sparky"; // create a variable 'dog' with the value 'Sparky'
-var dogAge = 5; // create a number variable
-var catAge = 2.5; // another a number
-
-var names = ["Sparky","Sheldon","Jasper"]; // create an array variable of names
-var ages = [2,5,3];
-
-//Objects
-var simpleCar = {
-    type : 'sedan',
-    color : 'red',
-    numberOfDoors : 4
-};
